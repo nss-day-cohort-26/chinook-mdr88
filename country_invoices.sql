297c389d3632930d10a45ac0d1fe6a84c1a6f215
mdroberts74 <mikedroberts88@gmail.com>
2018-09-24 09:47:27 -0500

    Provide a query that shows the # of invoices per country

---
+++ country_invoices.sql
@@ -0,0 +1,5 @@
+-- Provide a query that shows the # of invoices per country. HINT: GROUP BY
+
+
+SELECT COUNT(Invoice.InvoiceId), Invoice.BillingCountry FROM Invoice
+GROUP BY Invoice.BillingCountry;
