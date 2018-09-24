4159d7107ae04ae7cd989c3bc22273994d510fc8
mdroberts74 <mikedroberts88@gmail.com>
2018-09-24 09:51:01 -0500

    Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.

---
+++ playlists_track_count.sql
@@ -0,0 +1,5 @@
+-- Provide a query that shows the total number of tracks in each playlist. The Playlist name should be include on the resulant table.
+
+SELECT Playlist.Name, COUNT(PlaylistTrack.TrackId) FROM PlaylistTrack
+JOIN Playlist ON PlaylistTrack.PlaylistId = Playlist.PlaylistId
+GROUP BY Playlist.PlaylistId;
