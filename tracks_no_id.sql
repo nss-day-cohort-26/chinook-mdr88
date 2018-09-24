5824350ddedfce87b3605597ee903d63ded3b0b2
mdroberts74 <mikedroberts88@gmail.com>
2018-09-24 09:53:47 -0500

    Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre

---
+++ tracks_no_id.sql
@@ -0,0 +1,6 @@
+-- Provide a query that shows all the Tracks, but displays no IDs. The result should include the Album name, Media type and Genre.
+
+SELECT Track.Name, Album.Title, MediaType.Name, Genre.Name FROM Track
+JOIN Album ON Track.AlbumId = Album.AlbumId
+JOIN MediaType ON Track.MediaTypeId = MediaType.MediaTypeId
+JOIN Genre ON Track.GenreId = Genre.GenreId;
